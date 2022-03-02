689b4fa4493e385fb6ed2396e07db4a71ad8169f
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-02 09:15:56 +0300

    add comments on the differences between function literals, function statements and an arrow function

--- ArrowFunctions.js
+++ ArrowFunctions.js
@@ -1,2 +1,17 @@
+
+//this code works the same as the one below
+//this is an arrow function
 var add = (no1, no2) => no1+no2;
 console.log(add(93,21));
+
+//this is a function literal 
+var add = function(no1, no2){
+    return no1 +no2
+};
+console.log(add(31,14))
+
+//this is a function statement
+function add(no1, no2){
+    return no1 +no2
+};
+console.log(add(15,35))
