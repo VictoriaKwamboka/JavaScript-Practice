847b4487e16989cef08b5de074855065d83bb03a
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-02 12:20:12 +0300

    Add other examples of arrow functions

--- ArrowFunctions.js
+++ ArrowFunctions.js
@@ -1,5 +1,5 @@
 
-//this code works the same as the one below
+//this code works the same as the ones below
 //this is an arrow function
 var add = (no1, no2) => no1+no2;
 console.log(add(93,21));
@@ -15,3 +15,35 @@ function add(no1, no2){
     return no1 +no2
 };
 console.log(add(15,35))
+
+//function to print the length of elements
+
+var elements = ['Hydrogen','Helium','Lithium','Beryllium'];
+
+elements.map(function(element){
+  return element.length
+});
+
+
+//arrow alternative
+console.log(elements.map((element => element.length)));
+
+
+
+const hummus = function(factor){
+    const ingredient= function(amount, unit, comName){
+       let ingredientAmount= factor*amount;
+            if (ingredientAmount > 1) {
+                unit += 's'
+            }
+            console.log(`${ingredientAmount} ${amount} ${comName}`);
+        }
+        ingredient(1, 'can' ,'chickenpeas')
+        ingredient(0.25, 'cup', 'tahini')
+        ingredient(0.25, 'cup','lemon juice')
+    }
+
+console.log(hummus(2));
+
+
+
