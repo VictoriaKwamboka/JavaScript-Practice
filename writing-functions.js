7f5197e7c4bfac7a0b69e746e60dcff2bb62ad4b
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-02 08:28:30 +0300

    Add a function to subtract two numbers

--- writing-functions.js
+++ writing-functions.js
@@ -1,8 +1,18 @@
+
+//function to output an alert
 function makeNoise(){
     alert('meoooow')
 }
-
+//function to subtract 2 numbers 
 function subtract(myFirstNumber, mySecondNumber){
     return(myFirstNumber-mySecondNumber)
 }
 console.log(subtract(35,14))
+
+//function to add 2 variable number
+function add (no1, no2){
+    return no1+no2
+}
+var ten=10;
+var thirty=30;
+console.log(add(ten,thirty))
