5aa7273fcfb40538057510a27ab140349e2e88cc
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-02 12:50:09 +0300

    Add a function to subtract 2 numbers

---
+++ code-practice.js
@@ -0,0 +1,12 @@
+//function to take someone's name and say a greeting to them
+//var greeting = (myName) => alert('Hello '`${myName}`);
+//greeting('Victoria')
+
+
+
+
+
+
+//function to subtract two numbers
+var subtract = (no1, no2) => no1-no2;
+console.log(subtract(23,3))
