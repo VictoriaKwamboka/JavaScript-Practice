35069193a277a66ce8493b95599aee7afe6018e7
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-07 15:14:32 +0300

    Add a map function to add one to each array element

--- js/script.js
+++ js/script.js
@@ -9,9 +9,19 @@
 
 
 
-//map
-var numbers = [3,9,19,34];
-var tripledNumbers = numbers.map(function(number){
-    return number*3;
+//map to triple numbers
+
+// var numbers = [3,9,19,34];
+// var tripledNumbers = numbers.map(function(number){
+//     return number*3;
+// });
+// alert (tripledNumbers)
+
+
+//map to add one to each element of an array
+var numbers = [2,3,4,5,6,7]
+var addOne = numbers.map(function(number){
+    return number +1
 });
-alert (tripledNumbers)
+
+alert(addOne)
