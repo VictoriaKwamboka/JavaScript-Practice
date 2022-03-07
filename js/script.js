7a83d7c69ebf931a8025a8b0a96584affb8a63cf
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-07 15:04:15 +0300

    distinguish map and forEach methods

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-var total = 0;
-var summands = [1,2,3,4,5];
-summands.forEach(function(summand){
-    total += summand
+//forEach
+
+// var total = 0;
+// var summands = [1,2,3,4,5];
+// summands.forEach(function(summand){
+//     total += summand
+// });
+// console.log (total)
+
+
+
+//map
+var numbers = [3,9,19,34];
+var tripledNumbers = numbers.map(function(number){
+    return number*3;
 });
-console.log (total)
+alert (tripledNumbers)
