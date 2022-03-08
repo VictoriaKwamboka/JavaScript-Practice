6c73ebc72835b85e0f13d3c5efddb90ea56cab3f
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-08 08:10:00 +0300

    Add a for loop to iterate through an array as long as a condition is met

---
+++ for loops/for.js
@@ -0,0 +1,13 @@
+//increments the index by one whenever the condition evaluates to true
+
+// for (let index = 3; index<=4; index++){
+//     alert(index);
+// }
+
+
+//print an output
+
+fruits = 'mango passion banana'.split(" ")
+for (index = 0; index < fruits.length; index +=1){
+    alert('My favorite fruit is '+fruits[index]);
+}
