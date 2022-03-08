223ef98e454c987771c02089219b838a72a11815
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-08 08:21:15 +0300

    using for without arrays

--- for loops/for.js
+++ for loops/for.js
@@ -13,9 +13,17 @@
 // }
 
 //addition using for loop
+// let total = 0;
+// summands = [2,4,6,8]
+// for (let index = 0; index < summands.length; index ++){
+//     total += summands[index];
+// }
+// alert(total)
+
+//using for without arrays
+
 let total = 0;
-summands = [2,4,6,8]
-for (let index = 0; index < summands.length; index ++){
-    total += summands[index];
+for (let currentNumber = 0; currentNumber <= 4; currentNumber++){
+    total += currentNumber
 }
 alert(total)
