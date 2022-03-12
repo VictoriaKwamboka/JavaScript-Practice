78d7040f44e36631fc871d9f2e96272c0183ede3
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-12 09:15:47 +0300

    Add form input types radio , select, checkboxes, date and a color picker

---
+++ form-inputs/js/form.js
@@ -0,0 +1,16 @@
+
+//reading value selected from the select boxes
+let beverage = $('#beverage').val()
+//console.log(beverage);
+
+//reading values from radio buttons
+let flavor = $('input:radio[name=flavor]:checked').val()
+
+
+//getting date values
+let born = $('#born').val();
+//console.log(born)
+
+//color picker
+let color = $('#color').val()
+
